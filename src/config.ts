ddedcc8b4ef39d6d161459028920ef58a048a03d
Sixtus32 <sixtuswork18@gmail.com>
2022-10-15 20:01:58 +0200

    fix(meta): update website info

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE='skullwaves - homepage'
+export const NAME='skullwaves'
+export const SITE_DESCRIPTION='A crypto and tech junior life'
+export const HOMEPAGE_URL='https://www.skullwaves.io/'
