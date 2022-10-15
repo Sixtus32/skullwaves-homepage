9860e427ca1964544a13aaeca8e2fad3e43582c3
Sixtus32 <sixtuswork18@gmail.com>
2022-10-15 19:31:27 +0200

    chore(dep): add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import image from "@astrojs/image"
+import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), react(), image()]
+  integrations: [sitemap(), react(), image(), tailwind()]
 });
