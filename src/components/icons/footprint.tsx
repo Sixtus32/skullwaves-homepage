47571577f83ce9605e955e5e5693fd733ba071ba
Sixtus32 <sixtuswork18@gmail.com>
2022-10-16 01:58:04 +0200

    feat(header): build header and component

---
+++ src/components/icons/footprint.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function FootprintIcon(){
+  return (
+	<svg className="w-9 inline-block transition-transform group-hover:rotate-[-25deg]"
+      viewBox="0 0 40 40"
+    fill="currentColor"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2c5.523 0 10 4.477 10 10v3.764a2 2 0 0 1-1.106 1.789L18 19v1a3 3 0 0 1-2.824 2.995L14.95 23a2.5 2.5 0 0 0 .044-.33L15 22.5V22a2 2 0 0 0-1.85-1.995L13 20h-2a2 2 0 0 0-1.995 1.85L9 22v.5c0 .171.017.339.05.5H9a3 3 0 0 1-3-3v-1l-2.894-1.447A2 2 0 0 1 2 15.763V12C2 6.477 6.477 2 12 2zm-4 9a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/><g transform="matrix(1.02541,0.8,0.8,0.1001,2.55651,1.178215)">
+	<path fill="none" d="M0 0H24V24H0z"/><path d="M9 7.539L15 21.539 18.659 13 23 13 23 11 17.341 11 15 16.461 9 2.461 5.341 11 1 11 1 13 6.659 13z"/>
+    </g>
+</svg>)
+}
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE='skullwaves - homepage'
+export const SITE_TITLE='skullwaves'
 export const NAME='skullwaves'
 export const SITE_DESCRIPTION='A crypto and tech junior life'
 export const HOMEPAGE_URL='https://www.skullwaves.io/'
