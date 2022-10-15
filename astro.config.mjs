0d062a23ea2a998714651b253b1c209456b4a9d9
Sixtus32 <sixtuswork18@gmail.com>
2022-10-15 19:22:15 +0200

    chore(dep): add react and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import image from "@astrojs/image"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), react()]
+  integrations: [sitemap(), react(), image()]
 });
