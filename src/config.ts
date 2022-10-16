20997158f938b381159c680b96dd0f4dd21b3c2e
Sixtus32 <sixtuswork18@gmail.com>
2022-10-16 02:34:16 +0200

    feat(component): mainhead

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE='skullwaves'
+export const SITE_WEBNAME='skullwaves - homepage'
 export const NAME='skullwaves'
 export const SITE_DESCRIPTION='A crypto and tech junior life'
 export const HOMEPAGE_URL='https://www.skullwaves.io/'
